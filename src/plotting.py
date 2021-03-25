179ddff2660da5d951abce62357a0b9e95890190
Katerina Margatina <katerina.margatina@gmail.com>
2021-03-25 16:06:29 +0000

    added preliminary results (plots)

--- src/plotting.py
+++ src/plotting.py
@@ -21,22 +21,14 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 # from sys_config import FIG_DIR
 # from utilities.general import create_dir
 
-
-def uncertainty_plot(task_name, seeds, model_type='bert', learning_rate='2e-05', per_gpu_train_batch_size=16,
-                     num_train_epochs='3', indicators=None, methods=["vanilla", "mc3", "mc5", "mc10", "mc20", "temp_scale"]):
-
-    path = os.path.join(RES_DIR, '{}_{}_100%'.format(task_name, model_type))
-    if not os.path.exists(path): return
-
+def read_results_json(seeds, path, learning_rate, per_gpu_train_batch_size, num_train_epochs,
+                      indicators, methods, task_name, identity_init=False):
     df = pd.DataFrame()
-
-    if type(seeds) is not list: seeds = [seeds]
-    if type(indicators) is not list: indicators = [indicators]
-
     for seed in seeds:
         exp_path = os.path.join(path, 'seed_{}_lr_{}_bs_{}_epochs_{}'.format(seed, learning_rate,
                                                                             per_gpu_train_batch_size,
                                                                             int(num_train_epochs)))
+        if identity_init: exp_path += '_identity_init'
         if not os.path.exists(exp_path): pass
         df_ = pd.DataFrame()
         for ind in indicators:
@@ -50,8 +42,9 @@ def uncertainty_plot(task_name, seeds, model_type='bert', learning_rate='2e-05',
                     with open(res_path) as json_file:
                         results = json.load(json_file)
                         if ind is None: _ind = 'Base'
-                        if ind=='adapter': _ind = 'Base+Adapters'
-                        if ind=='bayes_adapter': _ind = 'Base+BayesAdapters'
+                        if ind=='adapter': _ind = 'Adapters'
+                        if ind=='bayes_adapter': _ind = 'BayesAdapters'
+                        if ind=='bayes_adapter' and identity_init: _ind = 'BayesAdapters+Identity'
 
                         val_results = results['val_results']
                         test_results = results['test_results']
@@ -76,6 +69,71 @@ def uncertainty_plot(task_name, seeds, model_type='bert', learning_rate='2e-05',
                     # df_['num_samples'] = num_samples
 
         df = df.append(df_, ignore_index=True)
+    return df
+
+def uncertainty_plot(task_name, seeds, model_type='bert', learning_rate='2e-05', per_gpu_train_batch_size=16,
+                     num_train_epochs='3', indicators=None, methods=["vanilla", "mc3", "mc5", "mc10", "mc20", "temp_scale"],
+                     identity_init=False):
+
+    path = os.path.join(RES_DIR, '{}_{}_100%'.format(task_name, model_type))
+    if not os.path.exists(path): return
+
+    df = pd.DataFrame()
+
+    if type(seeds) is not list: seeds = [seeds]
+    if type(indicators) is not list: indicators = [indicators]
+
+    df_ = read_results_json(seeds, path, learning_rate, per_gpu_train_batch_size, num_train_epochs,
+                            indicators, methods, task_name, identity_init=False)
+    df = df.append(df_, ignore_index=True)
+    if identity_init:
+        df_ = read_results_json(seeds, path, learning_rate, per_gpu_train_batch_size, num_train_epochs,
+                            indicators, methods, task_name, identity_init=True)
+        df = df.append(df_, ignore_index=True)
+    # for seed in seeds:
+    #     exp_path = os.path.join(path, 'seed_{}_lr_{}_bs_{}_epochs_{}'.format(seed, learning_rate,
+    #                                                                         per_gpu_train_batch_size,
+    #                                                                         int(num_train_epochs)))
+    #     if identity_init: exp_path += '_identity_init'
+    #     if not os.path.exists(exp_path): pass
+    #     df_ = pd.DataFrame()
+    #     for ind in indicators:
+    #         for method in methods:
+    #             res_filename = '{}_results'.format(method)
+    #             if ind is not None: res_filename += '_{}'.format(ind)
+    #             res_path = os.path.join(exp_path, "{}.json".format(res_filename))
+    #             if not os.path.exists(res_path):
+    #                 pass
+    #             else:
+    #                 with open(res_path) as json_file:
+    #                     results = json.load(json_file)
+    #                     if ind is None: _ind = 'Base'
+    #                     if ind=='adapter': _ind = 'Base+Adapters'
+    #                     if ind=='bayes_adapter': _ind = 'Base+BayesAdapters'
+    #
+    #                     val_results = results['val_results']
+    #                     test_results = results['test_results']
+    #                     df0 = pd.DataFrame(
+    #                         {'val_acc': val_results['acc'],
+    #                          'val_ece': val_results['ece']['ece'],
+    #                          'val_nll': val_results['nll']['mean'],
+    #                          'val_entropy': val_results['entropy']['mean'],
+    #                          'val_brier': val_results['brier']['mean'],
+    #                          'test_acc': test_results['acc'],
+    #                          'test_ece': test_results['ece']['ece'],
+    #                          'test_nll': test_results['nll']['mean'],
+    #                          'test_entropy': test_results['entropy']['mean'],
+    #                          'test_brier': test_results['brier']['mean'],
+    #                          'method': method,
+    #                          'indicator':_ind}, index=[0])
+    #                     df_ = df_.append(df0, ignore_index=True)
+    #
+    #                 # df_['val_acc'] = val_acc
+    #                 df_['dataset'] = task_name
+    #                 df_['seed'] = seed
+    #                 # df_['num_samples'] = num_samples
+    #
+    #     df = df.append(df_, ignore_index=True)
     # df = df_
     if not df.empty:
         # df['indicator'].loc[(df['indicator'] == None)] = 'Base'
@@ -116,6 +174,7 @@ def uncertainty_plot(task_name, seeds, model_type='bert', learning_rate='2e-05',
 
             plt.style.use("seaborn-colorblind")
             filename = "uncertainty_plot_{}".format(d)
+            if identity_init: filename += '_identity'
 
             # unc_dir = os.path.join(ex, 'uncertainty_plots')
             # create_dir(unc_dir)
@@ -139,8 +198,9 @@ if __name__ == '__main__':
     for dataset in datasets:
         # pass
         uncertainty_plot(task_name=dataset,
-                         seeds=[2,19,729],
+                         seeds=[2,19,729,982, 75, 281, 325, 195, 83, 4],
                          learning_rate='2e-05',
                          per_gpu_train_batch_size=32,
                          num_train_epochs='5',
-                         indicators=[None, 'adapter', 'bayes_adapter'])
+                         indicators=[None, 'adapter', 'bayes_adapter'],
+                         identity_init=True)
